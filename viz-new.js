1776b79b08a658919da89212930457adc4a762fd
jdjakub <16978710+jdjakub@users.noreply.github.com>
2021-09-24 23:05:06 +0100

    Distribute in the shrink case as well.

--- viz-new.js
+++ viz-new.js
@@ -78,23 +78,6 @@ function newGlue(toMin, toNat, toMax, stretch) {
   el('div', { class: 'spacer', style: width(advance) }, curr_line);
 }
 
-newWord('The');
-newGlue(10, 10, 20, 0);
-newWord('quick');
-newGlue(10, 10, 20, 0);
-newWord('brown');
-newGlue(10, 10, 20, 0);
-newWord('fox');
-
-curr_line = line2;
-newWord('The');
-newGlue(40, 10, 20, -30);
-newWord('quick');
-newGlue(40, 10, 20, -30);
-newWord('brown');
-newGlue(40, 10, 20, -30);
-newWord('fox');
-
 clwidth = e => e.getBoundingClientRect().width;
 Array.prototype.sum = function() {
   return this.reduce((acc, w) => acc + w,0);
@@ -107,30 +90,99 @@ function distribute(line) {
   const ideal_glue = Array.from(line.querySelectorAll('.min.glue, .nat.glue'))
     .map(clwidth).sum();
   const total_glue = total_space - total_words;
-  const excess_glue = total_glue - ideal_glue;
-  if (excess_glue >= 0.0) { // stretch
+  const total_stretch = total_glue - ideal_glue;
+  if (total_stretch >= 0.0) { // stretch
     const total_stretchability = Array.from(line.querySelectorAll('.max.glue'))
       .map(clwidth).sum();
     Array.from(line.querySelectorAll('.glue.item')).forEach(item => {
       const stretchability = clwidth(item.querySelector('.max.glue'));
       const proportion = stretchability / total_stretchability;
-      const actual_stretch = excess_glue * proportion;
+      const actual_stretch = total_stretch * proportion;
       // update stretch bar
-      item.querySelector('.stretch-shrink').style.width = actual_stretch+'px';
+      const bar = item.querySelector('.stretch-shrink');
+      bar.parentElement.classList.add('stretch');
+      bar.parentElement.classList.remove('shrink');
+      bar.style.width = actual_stretch+'px';
+      bar.style.right = null;
+      // update .spacer
       const nat_glue = clwidth(item.querySelector('.min.glue'))
                      + clwidth(item.querySelector('.nat.glue'));
       const actual_glue = nat_glue + actual_stretch;
-      // update .spacer
       item.parentElement.nextElementSibling.style.width = actual_glue+'px';
     });
     const stretch_amt = line.querySelector('.stretch-amt');
     const denom = line.querySelector('.denom');
     const quotient = line.querySelector('.quotient');
-    stretch_amt.textContent = Math.round(excess_glue);
+    stretch_amt.textContent = Math.round(total_stretch);
     denom.textContent = Math.round(total_stretchability);
-    const ratio = excess_glue / total_stretchability;
+    const ratio = total_stretch / total_stretchability;
     quotient.textContent = ratio.toPrecision(2);
   } else { // shrink
-    throw "Oops";
+    const total_shrink = -total_stretch;
+    const total_shrinkability = Array.from(line.querySelectorAll('.nat.glue'))
+      .map(clwidth).sum();
+    Array.from(line.querySelectorAll('.glue.item')).forEach(item => {
+      const shrinkability = clwidth(item.querySelector('.nat.glue'));
+      const proportion = shrinkability / total_shrinkability;
+      const actual_shrink = total_shrink * proportion;
+      // update shrink bar
+      const bar = item.querySelector('.stretch-shrink');
+      bar.parentElement.classList.add('shrink');
+      bar.parentElement.classList.remove('stretch');
+      bar.style.width = actual_shrink+'px';
+      bar.style.right = '0px';
+      // update .spacer
+      const nat_glue = clwidth(item.querySelector('.min.glue'))
+                     + clwidth(item.querySelector('.nat.glue'));
+      const actual_glue = nat_glue - actual_shrink;
+      item.parentElement.nextElementSibling.style.width = actual_glue+'px';
+    });
+    const shrink_amt = line.querySelector('.shrink-amt');
+    const denom = line.querySelector('.denom');
+    const quotient = line.querySelector('.quotient');
+    shrink_amt.textContent = Math.round(total_stretch);
+    denom.textContent = Math.round(total_shrinkability);
+    const ratio = total_stretch / total_shrinkability;
+    quotient.textContent = ratio.toPrecision(2);
   }
 }
+
+newWord('The');
+newGlue(10, 10, 10, 0);
+newWord('quick');
+newGlue(10, 10, 10, 0);
+newWord('brown');
+newGlue(10, 10, 10, 0);
+newWord('fox');
+newGlue(10, 10, 40, 0);
+newWord('jumped');
+newGlue(10, 10, 40, 0);
+newWord('over');
+newGlue(10, 10, 10, 0);
+newWord('the');
+newGlue(10, 10, 10, 0);
+newWord('lazy');
+newGlue(10, 10, 10, 0);
+newWord('dog.');
+
+curr_line = line2;
+newWord('The');
+newGlue(20, 20, 20, -10);
+newWord('quick');
+newGlue(20, 20, 20, -10);
+newWord('brown');
+newGlue(20, 20, 20, -10);
+newWord('fox');
+newGlue(20, 30, 20, -10);
+newWord('jumped');
+newGlue(25, 30, 20, -10);
+newWord('over');
+newGlue(20, 20, 20, -10);
+newWord('the');
+newGlue(20, 20, 20, -10);
+newWord('lazy');
+newGlue(20, 20, 20, -10);
+newWord('dog.');
+
+distribute(line1)
+distribute(line2)
