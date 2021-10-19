67303f51a037a87b6181fe8ee3f3fcfdbe0256da
jdjakub <16978710+jdjakub@users.noreply.github.com>
2021-10-19 19:31:32 +0100

    Lay out words from string with first-fit and local best-fit based on stretch ratio

--- viz-new.js
+++ viz-new.js
@@ -49,17 +49,23 @@ create_element = (tag, attrs, parent, namespace) => {
 // e.g. div = el('div', { ??? }, body)
 el = (tag, attrs, parent) => create_element(tag, attrs, parent, 'http://www.w3.org/1999/xhtml');
 
-line1 = document.getElementById('line1');
-line2 = document.getElementById('line2');
-curr_line = line1;
+lines = document.getElementById('lines');
+curr_line = undefined;
+
+width = x => 'width: ' + x + 'px';
+
+function newLine() {
+  curr_line = el('div', { class: 'line' }, lines);
+  el('div', { class: 'desired-line', style: width(600) }, curr_line);
+  const ratio = el('span', { class: 'ratio' }, curr_line);
+  ratio.innerHTML = '<span class="stretch-amt numer">300</span>/<span class="denom">60</span> = <span class="quotient">5.0</span>';
+}
 
 function newWord(str) {
   const div = el('div', { class: 'word item' }, curr_line);
   el('div', { class: 'text', textContent: str }, div);
   el('div', { class: 'rigid' }, div);
 }
-
-width = x => 'width: ' + x + 'px';
 
 function newGlue(toMin, toNat, toMax, stretch) {
   const item = el('div', { class: 'glue item' }, el('div', { class: 'container' }, curr_line));
@@ -110,13 +116,15 @@ function distribute(line) {
       const actual_glue = nat_glue + actual_stretch;
       item.parentElement.nextElementSibling.style.width = actual_glue+'px';
     });
-    const stretch_amt = line.querySelector('.stretch-amt');
+    const stretch_amt = line.querySelector('.numer');
     const denom = line.querySelector('.denom');
     const quotient = line.querySelector('.quotient');
     stretch_amt.textContent = Math.round(total_stretch);
+    stretch_amt.classList.add('stretch-amt'); stretch_amt.classList.remove('shrink-amt');
     denom.textContent = Math.round(total_stretchability);
     const ratio = total_stretch / total_stretchability;
     quotient.textContent = ratio.toPrecision(2);
+    return ratio;
   } else { // shrink
     const total_shrink = -total_stretch;
     const total_shrinkability = Array.from(line.querySelectorAll('.nat.glue'))
@@ -137,52 +145,79 @@ function distribute(line) {
       const actual_glue = nat_glue - actual_shrink;
       item.parentElement.nextElementSibling.style.width = actual_glue+'px';
     });
-    const shrink_amt = line.querySelector('.shrink-amt');
+    const shrink_amt = line.querySelector('.numer');
     const denom = line.querySelector('.denom');
     const quotient = line.querySelector('.quotient');
     shrink_amt.textContent = Math.round(total_stretch);
+    shrink_amt.classList.add('shrink-amt'); shrink_amt.classList.remove('stretch-amt');
     denom.textContent = Math.round(total_shrinkability);
     const ratio = total_stretch / total_shrinkability;
     quotient.textContent = ratio.toPrecision(2);
+    return ratio;
   }
 }
 
-newWord('The');
-newGlue(10, 10, 10, 0);
-newWord('quick');
-newGlue(10, 10, 10, 0);
-newWord('brown');
-newGlue(10, 10, 10, 0);
-newWord('fox');
-newGlue(10, 10, 40, 0);
-newWord('jumped');
-newGlue(10, 10, 40, 0);
-newWord('over');
-newGlue(10, 10, 10, 0);
-newWord('the');
-newGlue(10, 10, 10, 0);
-newWord('lazy');
-newGlue(10, 10, 10, 0);
-newWord('dog.');
+paragraph = `
+In olden times when wishing still helped one, there lived a king whose daughters
+were all beautiful; and the youngest was so beautiful that the sun itself, which
+has seen so much, was astonished whenever it shone in her face. Close by the
+king’s castle lay a great dark forest, and under an old lime-tree in the forest
+was a well, and when the day was very warm, the king’s child went out into the
+forest and sat down by the side of the cool fountain; and when she was bored she
+took a golden ball, and threw it up on high and caught it; and this ball was her
+favorite plaything.
+`;
+words = paragraph.trim().split('\n').flatMap(l => l.split(' '));
+std_glue = [10, 20, 20, 0];
 
-curr_line = line2;
-newWord('The');
-newGlue(20, 20, 20, -10);
-newWord('quick');
-newGlue(20, 20, 20, -10);
-newWord('brown');
-newGlue(20, 20, 20, -10);
-newWord('fox');
-newGlue(20, 30, 20, -10);
-newWord('jumped');
-newGlue(25, 30, 20, -10);
-newWord('over');
-newGlue(20, 20, 20, -10);
-newWord('the');
-newGlue(20, 20, 20, -10);
-newWord('lazy');
-newGlue(20, 20, 20, -10);
-newWord('dog.');
+let next_i = 0;
 
-distribute(line1)
-distribute(line2)
+function next(doBreak=false) {
+  if (1+next_i > words.length) return;
+  if (doBreak) newLine();
+  else newGlue(...std_glue);
+  newWord(words[next_i++]);
+  return distribute(curr_line);
+}
+
+function undo() {
+  const word = curr_line.querySelector('.word.item:last-child');
+  if (word) {
+    word.previousElementSibling.remove(); // spacer
+    word.previousElementSibling.remove(); // glue
+    word.remove();
+    next_i--;
+  }
+  return distribute(curr_line);
+}
+
+// To be used with ratio between -1 and 1
+function mark_feasible() {
+  const word = curr_line.querySelector('.word.item:last-child');
+  if (word) {
+    word.querySelector('.rigid').classList.toggle('feasible');
+  }
+}
+
+function first_fit() {
+  while (true) {
+    next(true);
+    let ratio;
+    do { ratio = next(); } while (ratio !== undefined && ratio > 1.0);
+    if (ratio === undefined) break;
+  }
+  newGlue(0,0,10000,0); distribute(curr_line) // end paragraph
+}
+
+function best_fit() {
+  while (true) {
+    next(true);
+    let ratio;
+    do { ratio = next(); } while (ratio !== undefined && ratio >= 0.0);
+    if (ratio === undefined) break;
+    const ratio2 = undo(); // wait! go back a sec (to stretch case)
+    if (-ratio < ratio2) next(); // shrunk is better; restore shrunk line
+    // otherwise stretched is better
+  }
+  newGlue(0,0,10000,0); distribute(curr_line) // end paragraph
+}
